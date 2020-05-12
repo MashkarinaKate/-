5fc0c46865e324dccfc48f25400162799811414c
MashkarinaKate <mashkarinakate@gmail.com>
2020-05-12 19:29:43 +0300

    мили-километры

---
+++ практика 2 по окнам мили-километры/практика 2 по окнам мили-километры/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace практика_2_по_окнам_мили_километры
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                label2.Text = "Указанное число миль равно: " + double.Parse(textBox1.Text) * 1.61 + "км";
+            else label2.Text = "Вы не указали число милей";
+
+        }
+    }
+}
---
+++ практика 2 по окнам мили-километры/практика 2 по окнам мили-километры/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace практика_2_по_окнам_мили_километры
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
